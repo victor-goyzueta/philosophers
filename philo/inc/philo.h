e95302b9834f0c0069764e1160985b3d10568c08
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-07-21 10:40:16 +0200

    update

---
+++ philo/inc/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 10:29:47 by vgoyzuet          #+#    #+#             */
+/*   Updated: 2025/07/21 10:37:15 by vgoyzuet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
